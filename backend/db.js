1689ca83a136f7c62cfb6ef637e2ed42302a5e43
Jose Joy <123643609+josejoy2216@users.noreply.github.com>
2024-05-14 22:48:38 +0530

    backend

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const url = 
+"mongodb+srv://josejoy567:josejoy567@cluster0.llpaxc1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+module.exports.connect = () =>{
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(()=>{
+        console.log("MongoDB Connected Successfully")
+    }).catch((error) => { console.log("Error: " , error) })
+};
+
